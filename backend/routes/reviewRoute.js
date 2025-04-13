2946e5c04d127095e1a04947c81f5ec9e393e829
lakshaythapa08 <lakshay.thapa08@gmail.com>
2025-04-13 22:00:09 +0530

    fixing logic in dark mode

--- backend/routes/reviewRoute.js
+++ backend/routes/reviewRoute.js
@@ -5,7 +5,6 @@ const getGeminiReview = require('../utils/gemini');
 router.post('/', async (req, res) => {
   let {code}  = req.body;
   code=code.trim();
-  console.log(code.length);
 
   if (!code || code.length==0) {
     return res.status(400).json({ error: 'Code is required' });
